d0a78c37f8b9e811ca8d3d1bdafe6fa140b17638
masud <abdullahalmasud01737906772@gmail.com>
2022-04-04 14:36:16 +0600

    add blogs question & ans, logo & site name

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,44 +1,72 @@
 import React from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../../node_modules/bootstrap/dist/js/bootstrap.min';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div className='container'>
+        <div className='container mt-5 bl-bg-color'>
             <div class="accordion" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                            Accordion Item #1
+                            What is context api?
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
                         <div class="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <p>
+                                <strong>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</strong> The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                            <p>
+                                Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease.
+                                Context provides a way to pass data through the component tree without having to pass props down manually at every level.
+
+                            </p>
+                            <p>
+                                In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                            Accordion Item #2
+                            What is semantics tag ?
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                         <div class="accordion-body">
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <p>
+                                <strong>Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page.</strong>
+                            </p>
+                            <p>A semantic element clearly describes its meaning to both the browser and the developer.</p>
+                            <p><strong>Examples of non-semantic elements:</strong> "div" and "span" Tells nothing about its content.</p>
+                            <p><strong>Examples of semantic elements:</strong> "form", "table" and "article" Clearly defines its content.</p>
+                            <p>
+                                <strong>
+                                    Why You Should Care About Semantics: </strong>
+                                The benefit of writing semantic HTML stems from what should be the driving goal of any web page: the desire to communicate. By adding semantic tags to your document, you provide additional information about that document, which aids in communication. Specifically, semantic tags make it clear to the browser what the meaning of a page and its content is. That clarity is also communicated with search engines, ensuring that the right pages are delivered for the right queries.
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="panelsStayOpen-headingThree">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                            Accordion Item #3
+                            What is the difference between inline block and inline block elements?
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                         <div class="accordion-body">
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <p>Compared to display: inline , the major difference is that <strong>inline-block allows to set a width and height on the element.</strong> Also, with display: inline , top and bottom margins and paddings are not respected, and with display: inline-block they are.
+                            </p>
+                            <p>
+                                display: inline-block brought a new way to create side by side boxes that collapse and wrap properly depending on the available space in the containing element. It makes layouts that were previously accomplished with floats easier to create. No need to clear floats anymore.
+
+                                Compared to display: inline, the major difference is that inline-block allows to set a width and height on the element. Also, with display: inline, top and bottom margins and paddings are not respected, and with display: inline-block they are.
+
+                                Now, the difference between display: inline-block and display: block is that, with display: block, a line break happens after the element, so a block element doesn’t sit next to other elements.
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,8 +6,8 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
     return (
-        <div>
-            <div className='laptop-info mt-5 container'>
+        <div className='ho-bg-color'>
+            <div className='laptop-info mt-5 container btn-demo'>
                 <div>
                     <h1 class="card-title">The world most powerful and Beautiful laptop</h1>
                     <p class="">Laptops are evolving at a dramatic pace, and this is the place to track their progress, or lack thereof. The best laptop is out there, and our laptop reviews dig deep into what’s new from the world’s biggest manufacturers to help you find it. From each new iteration of the Apple MacBook and Microsoft Surface to what’s coming up from likes of Dell, HP, Lenovo, and more, The Verge has you covered.</p>
